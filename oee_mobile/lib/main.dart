87c7e2418f8c2eaf53775537d6141476b96e0d3d
billran <kentra@pacbell.net>
2019-12-27 18:45:29 -0800

    wip

--- oee_mobile/lib/main.dart
+++ oee_mobile/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 //import 'package:http/http.dart' as http;
 //import 'package:oee_mobile/TreeViewMain.dart';
 //import 'package:dynamic_treeview/dynamic_treeview.dart';
+//import 'TreeViewDataModel.dart';
 import 'dynamic_treeview.dart';
 import 'model.dart';
 import 'EquipmentEvent.dart';
@@ -60,11 +61,17 @@ class OeeHomePage extends StatefulWidget {
 class _OeeHomePageState extends State<OeeHomePage> {
   int _counter = 0;
   Future<MaterialList> materialListFuture;
+  List<BaseData> materialData;
 
   @override
   void initState() {
     super.initState();
-    //post = fetchPost();
+
+    fetchMaterials();
+  }
+
+  void fetchMaterials() {
+    materialListFuture = OeeHttpService.fetchMaterials();
   }
 
   void _incrementCounter() {
@@ -84,36 +91,36 @@ class _OeeHomePageState extends State<OeeHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      body: FutureBuilder<MaterialList>(
+        future: materialListFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            MaterialList matList = snapshot.data;
+            OeeMaterial dto = matList.materialList[0];
+            //return Text(dto.name);
+            Widget widget = this.createMaterialView(matList);
+            //widget = Text(dto.name);
+            return widget;
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          // By default, show a loading spinner.
+          return CircularProgressIndicator();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+
+      /*
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -123,178 +130,124 @@ class _OeeHomePageState extends State<OeeHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+
             FutureBuilder<MaterialList>(
               future: materialListFuture,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   MaterialList matList = snapshot.data;
-                  OeeMaterial dto = matList.materialList[_counter];
-                  return Text(dto.name);
+                  OeeMaterial dto = matList.materialList[0];
+                  //return Text(dto.name);
+                  Widget widget =  this.createMaterialView(matList);
+                  widget = Text(dto.name);
+                  return widget;
                 } else if (snapshot.hasError) {
                   return Text("${snapshot.error}");
                 }
                 // By default, show a loading spinner.
                 return CircularProgressIndicator();
               },
-            ),
-            DynamicTreeView(
-              data: getData(),
-              config: Config(
-                  parentTextStyle: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w600),
-                  rootId: "1",
-                  parentPaddingEdgeInsets:
-                      EdgeInsets.only(left: 16, top: 0, bottom: 0)),
-              onTap: (m) {
-                print("onChildTap -> $m");
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (ctx) => EquipmentEvent(
-                              data: m,
-                            )));
-              },
-              width: MediaQuery.of(context).size.width,
-            ),
+            )
           ],
         ),
-      ),
+      )
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+
+       */
     );
   }
 
-  List<BaseData> getData() {
-    //if (materialListFuture == null) {
-      materialListFuture = OeeHttpService.fetchMaterials();
-      //this.materialListFuture.whenComplete(() => print("i am done"));
-      materialListFuture.then((value) {
-        print('completed with value $value');
-      }, onError: (error) {
-        print('completed with error $error');
-      });
-    //}
+  DynamicTreeView createMaterialView(MaterialList materialList) {
+    return DynamicTreeView(
+      data: fromMaterialList(materialList),
+      config: Config(
+          parentTextStyle:
+              TextStyle(color: Colors.black, fontWeight: FontWeight.w600),
+          rootId: "root",
+          parentPaddingEdgeInsets:
+              EdgeInsets.only(left: 16, top: 0, bottom: 0)),
+      onTap: (m) {
+        //print("onChildTap -> $m");
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (ctx) => EquipmentEvent(
+                      data: m,
+                    )));
+      },
+      width: MediaQuery.of(context).size.width,
+    );
+  }
 
-    return [
-      MaterialDataModel(
-        id: 1,
-        name: 'Root',
-        parentId: -1,
-        extras: {'key': 'extradata1'},
-      ),
-      MaterialDataModel(
-        id: 2,
-        name: 'Men',
-        parentId: 1,
-        extras: {'key': 'extradata2'},
-      ),
-      MaterialDataModel(
-        id: 3,
-        name: 'Shorts',
-        parentId: 2,
-        extras: {'key': 'extradata3'},
-      ),
-      MaterialDataModel(
-        id: 4,
-        name: 'Shoes',
-        parentId: 2,
-        extras: {'key': 'extradata4'},
-      ),
-      MaterialDataModel(
-        id: 5,
-        name: 'Women',
-        parentId: 1,
-        extras: {'key': 'extradata5'},
-      ),
-      MaterialDataModel(
-        id: 6,
-        name: 'Shoes',
-        parentId: 5,
-        extras: {'key': 'extradata6'},
-      ),
-      MaterialDataModel(
-        id: 7,
-        name: 'Shorts',
-        parentId: 5,
-        extras: {'key': 'extradata7'},
-      ),
-      MaterialDataModel(
-        id: 8,
-        name: 'Tops',
-        parentId: 5,
-        extras: {'key': 'extradata8'},
-      ),
-      MaterialDataModel(
-        id: 9,
-        name: 'Electronics',
-        parentId: 1,
-        extras: {'key': 'extradata9'},
-      ),
-      MaterialDataModel(
-        id: 10,
-        name: 'Phones',
-        parentId: 9,
-        extras: {'key': 'extradata10'},
-      ),
-      MaterialDataModel(
-        id: 11,
-        name: 'Tvs',
-        parentId: 9,
-        extras: {'key': 'extradata11'},
-      ),
-      MaterialDataModel(
-        id: 12,
-        name: 'Laptops',
-        parentId: 9,
-        extras: {'key': 'extradata12'},
-      ),
-      MaterialDataModel(
-        id: 13,
-        name: 'Nike shooes',
-        parentId: 4,
-        extras: {'key': 'extradata13'},
-      ),
-      MaterialDataModel(
-        id: 14,
-        name: 'puma shoes',
-        parentId: 4,
-        extras: {'key': 'extradata14'},
-      ),
-      MaterialDataModel(
-        id: 15,
-        name: 'puma shoes 1',
-        parentId: 14,
-        extras: {'key': 'extradata15'},
-      ),
-      MaterialDataModel(
-        id: 16,
-        name: 'puma shoes 2',
-        parentId: 14,
-        extras: {'key': 'extradata16'},
-      ),
-      MaterialDataModel(
-        id: 17,
-        name: 'puma shoes 3',
-        parentId: 14,
-        extras: {'key': 'extradata17'},
-      ),
-    ];
+  List<BaseData> getMaterialData() {
+    //if (materialData == null) {
+    materialListFuture = OeeHttpService.fetchMaterials();
+    //this.materialListFuture.whenComplete(() => print("i am done"));
+    materialListFuture.then((value) {
+      materialData = fromMaterialList(value);
+    }, onError: (error) {
+      print('completed with error $error');
+    });
+    //}
+    return materialData;
+  }
+
+  List<BaseData> fromMaterialList(MaterialList materialList) {
+    List<BaseData> materialData = List();
+    // root
+    OeeMaterial rootMaterial = OeeMaterial("root", "root", null);
+    MaterialDataModel root = MaterialDataModel(rootMaterial);
+    materialData.add(root);
+
+    List<String> categories = List();
+
+    // materials
+    List<OeeMaterial> materials = materialList.materialList;
+    for (OeeMaterial material in materials) {
+      MaterialDataModel matModel = MaterialDataModel(material);
+
+      if (!categories.contains(material.category)) {
+        categories.add(material.category);
+        matModel.parentId = material.category;
+      }
+      materialData.add(matModel);
+    }
+
+    // add categories
+    for (String category in categories) {
+      OeeMaterial catMaterial = OeeMaterial(category, category, "root");
+      MaterialDataModel cat = MaterialDataModel(catMaterial);
+      materialData.add(cat);
+    }
+    return materialData;
   }
 }
 
 class MaterialDataModel implements BaseData {
-  final int id;
-  final int parentId;
+  static const String MAT_KEY = "material";
+
+  String id;
+  String parentId;
   String name;
   String subTitle;
   Icon icon;
 
   ///Any extra data you want to get when tapped on children
   Map<String, dynamic> extras;
-  MaterialDataModel({this.id, this.parentId, this.name, this.extras});
+
+  MaterialDataModel(OeeMaterial material) {
+    this.parentId = material.category;
+    this.id = material.name;
+    this.name = material.name;
+
+    extras = {MAT_KEY: material};
+  }
+
+  //MaterialDataModel({this.id, this.parentId, this.name, this.extras});
   @override
   String getId() {
     return this.id.toString();
